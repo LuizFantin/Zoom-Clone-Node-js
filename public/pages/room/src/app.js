b88264cb03ceddf84da91e870e17ecfe4029d020
Luiz Fantin <fantin_luiz@outlook.com>
2021-02-18 14:14:54 -0300

    changes for Heroku Deploy

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -3,14 +3,17 @@ const onload = () => {
   const room = urlParams.get('room');
   console.log('this is the room', room)
 
-  const socketUrl = 'http://localhost:3000'
+  // const socketUrl = 'http://localhost:3000'
+  const socketUrl = 'https://pacific-fjord-68079.herokuapp.com'
   const socketBuilder = new SocketBuilder({socketUrl})
 
   const peerConfig = Object.values({
     id: undefined,
     config: {
-      port: 9000,
-      host: 'localhost',
+      host: 'nameless-coast-38899.herokuapp.com',
+      secure: true,
+      // port: 9000,
+      // host: 'localhost',
       path: '/'
     }
   })
--- public/pages/room/src/view.js
+++ public/pages/room/src/view.js
@@ -73,7 +73,7 @@ class View{
             
             command()
 
-            await Util.sleep(1000)
+            await Util.sleep(3000)
             window.location = '/pages/home'
         }
     }
